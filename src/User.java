463317a105e883646912e655e2383a7897ca6d1d
alphanout <atishay818@gmail.com>
2021-05-06 11:45:57 +0530

    added jdbc

--- src/User.java
+++ src/User.java
@@ -1,50 +1,49 @@
 package src;
+
 import java.util.*;
+
 public class User {
     int uid;
     String uname;
-    User(int uid, String uname, Scanner sc)
-    {
-        this.uid=uid;
-        this.uname=uname;
+
+    User(int uid, String uname, Scanner sc) {
+        this.uid = uid;
+        this.uname = uname;
         System.out.println("User Successfully Registered");
         gameSelection(uid, sc);
     }
-    private void gameSelection(int uid, Scanner sc)
-    {
-        while(true)
-        {
+
+    private void gameSelection(int uid, Scanner sc) {
+        while (true) {
             System.out.println("Welcome, we have 3 games with us for now.");
             System.out.println("1 - Jumbled Words");
             System.out.println("2 - Riddler");
             System.out.println("3 - Hangman");
             System.out.println("Enter your choice or Enter 0 to exit");
             int choice = sc.nextInt();
-            switch(choice)
-            {
+            switch (choice) {
                 case 1:
-                System.out.println("First Game running");
-                //constructor calling for first game
-                break;
+                    System.out.println("First Game running");
+                    // constructor calling for first game
+                    break;
                 case 2:
-                System.out.println("Second Game running");
-                //constructor calling for 2nd game
-                break;
+                    System.out.println("Second Game running");
+                    // constructor calling for 2nd game
+                    break;
                 case 3:
-                System.out.println("Third Game running");
-                Hangman h = new Hangman(sc);
-                break;
+                    System.out.println("Third Game running");
+                    new Hangman(sc);
+                    break;
                 case 0:
-                System.exit(0);
+                    System.exit(0);
                 default:
-                System.out.println("Wrong Input!! Please enter again");
-                break;
+                    System.out.println("Wrong Input!! Please enter again");
+                    break;
             }
         }
     }
-    private void getHistory()
-    {
 
-    }   
+    private void getHistory() {
+
+    }
 }
-
