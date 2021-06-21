649535c3130f1b3d5d3c4f08da2f6bcd1647f681
PriyaSKulkarni <67280246+PriyaSKulkarni@users.noreply.github.com>
2021-06-21 14:50:05 +0530

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,9 +1,10 @@
+package src;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.*;
-public class User 
-{
+public class User {
     int uid;
     String uname;
     int res;
@@ -11,55 +12,56 @@ public class User
     {
     	if(uid==0) {
     		System.out.println("Enter your choice:");
-    		while(true) 
-            {
+    		while(true) {
     			System.out.println("1 - Perform Operations ");
     			System.out.println("2 - Play game");
-                System.out.println("3 - LOGOUT");
+    			System.out.println("3 - Logout");
     			int ch=sc.nextInt();
     			switch(ch) {
     			case 1:
-    				new Admin();
+    				Admin a=new Admin();
     				break;
     			case 2:
     				gameSelection(0,sc);
     				break;
-                case 3:
-                    App a = new App();
-                    a.userRegister();
+    			case 3:
+				System.out.println("Thank you for playing the game");
+    				App z=new App();
+    				z.userRegister();
+    				break;
     			default:
     				System.out.println("Enter correct choice");
     				break;
-    			}	
+    			}
     		}
     	}
-    	else 
-        {
+    	else {
         gameSelection(uid,sc);
+        System.out.println("Thank you for playing the game");
+        App az=new App();
+        az.userRegister();
     	}
     }
     User(int uid, String uname, Scanner sc)
     {
         this.uid=uid;
         this.uname=uname;
-        try 
-        {
-        	Connection con = Conn.getInstance();
+        
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/word_waffle","root","Priya@1508");  
             Statement stmt=con.createStatement(); 
             String sql1="insert into user values("+uid+",'"+uname+"')";
-            res=stmt.executeUpdate(sql1);
+             res=stmt.executeUpdate(sql1);
         }
-        catch(Exception e) 
-        {
+        catch(Exception e) {
         	System.out.println(e);
         }
-        if(res==1) 
-        {
-            System.out.println("User Successfully Registered");
-            gameSelection(uid, sc);
+        if(res==1) {
+        System.out.println("User Successfully Registered");
+        gameSelection(uid, sc);
         }
-        else 
-        {
+        else {
         	System.out.println("Duplicate id");
         	App a=new App();
         	a.userRegister();
@@ -74,7 +76,7 @@ public class User
             System.out.println("2 - Riddler");
             System.out.println("3 - Hangman");
             System.out.println("4 - View History");
-            System.out.println("5 - LOGOUT");
+            System.out.println("5 - Quit");
             System.out.println("Enter your choice or Enter 0 to exit");
             int choice = sc.nextInt();
             sc.nextLine();
@@ -90,48 +92,50 @@ public class User
                 new Hangman(sc,uid);
                 break;
                 case 4:
-                getHistory(uid);
-                break;
-                case 5:
-                App a = new App();
-                a.userRegister();
+                	getHistory(uid);
+                	break;
                 case 0:
                 System.exit(0);
+                case 5:
+                	break;
                 default:
                 System.out.println("Wrong Input!! Please enter again");
                 break;
             }
+            if(choice==5)
+            	break;
         }
     }
-    private static void getHistory(int uid)
-    {
-    	try 
-        {
-    		Connection con=Conn.getInstance();
- 	        Statement stmt=con.createStatement();
- 	        String sqli="Select * from user_logs where U_id="+uid+";";
- 	        ResultSet rs=stmt.executeQuery(sqli);
-            if(rs.next())
+    private static void getHistory(int uid){
+    	
+    		try 
             {
-                System.out.println("U_ID --- Score --- Date/Time --- Game Name ");  
-                while(rs.next()) 
+        		Connection con=Conn.getInstance();
+     	        Statement stmt=con.createStatement();
+     	        String sqli="Select * from user_logs where UID_id="+uid+";";
+     	        ResultSet rs=stmt.executeQuery(sqli);
+                if(rs.next())
                 {
-	    	        for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) 
+                    System.out.println("U_ID ------ Total-Score --- Date/Time --- Game Name ");  
+                    while(rs.next()) 
                     {
-	                System.out.print(rs.getObject(i)+" --- ");
-	                }
-	                System.out.println("");
-	            }
-            }
-            else
+    	    	        for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) 
+                        {
+    	                System.out.print(rs.getObject(i)+" --- ");
+    	                }
+    	                System.out.println("");
+    	            }
+                }
+                else
+                {
+                    System.out.println("No History Available!! Play some games first.");
+                }
+        	}
+        	catch(Exception e) 
             {
-                System.out.println("No History Available!! Play some games first.");
-            }
-    	}
-    	catch(Exception e) 
-        {
-    		System.out.println(e);
-    	}
-    }     
+        		System.out.println(e);
+        	}
+    }
+     
 }
 
