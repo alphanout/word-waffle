d7a132a5b5ea6d11be025a363ff0c0f2ff82c526
PriyaSKulkarni <67280246+PriyaSKulkarni@users.noreply.github.com>
2021-06-21 11:22:53 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,12 +1,16 @@
-package PW;
+package src;
 import java.util.*;
-public class App 
-{
+public class App {
     public static void main(String[] args) throws Exception {
+       App a1=new App();
+       a1.userRegister();
+    }
+        void userRegister() {
         int uid;
         String username;
         Scanner sc= new Scanner(System.in);
-        System.out.println("Enter 1 for New USER \nEnter 2 for Registered USER");
+        System.out.println("WELCOME TO WORD-WAFFLE GAME");
+        System.out.println("Enter 1 for New USER \n Enter 2 for Registered USER");
         int choice = sc.nextInt();
         switch(choice)
         {
@@ -15,13 +19,13 @@ public class App
             uid = sc.nextInt();
             System.out.println("Enter your Name");
             username = sc.next();
-            new User(uid, username,sc);
+            User u = new User(uid, username,sc);
             break;
             case 2 :
             System.out.println("Welcome Back!!!, Please enter your UserID");
             uid = sc.nextInt();
-            new User(uid, sc);
+            User s = new User(uid, sc);
             break;
-        }
     }
+}
 }
