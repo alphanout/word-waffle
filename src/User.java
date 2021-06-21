faae61f038bfae6b121c31aed6b92623c6ec315e
PriyaSKulkarni <67280246+PriyaSKulkarni@users.noreply.github.com>
2021-06-21 11:26:18 +0530

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,18 +1,62 @@
-package PW;
+package src;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.*;
 public class User {
     int uid;
     String uname;
+    int res;
     User(int uid, Scanner sc)
     {
+    	if(uid==0) {
+    		System.out.println("Enter your choice:");
+    		while(true) {
+    			System.out.println("1 - Perform Operations ");
+    			System.out.println("2 - Play game");
+    			int ch=sc.nextInt();
+    			switch(ch) {
+    			case 1:
+    				Admin a=new Admin();
+    				break;
+    			case 2:
+    				gameSelection(0,sc);
+    				break;
+    			default:
+    				System.out.println("Enter correct choice");
+    				break;
+    			}
+    			
+    		}
+    	}
+    	else {
         gameSelection(uid,sc);
+    	}
     }
     User(int uid, String uname, Scanner sc)
     {
         this.uid=uid;
         this.uname=uname;
+        
+        try {
+        	Class.forName("com.mysql.cj.jdbc.Driver");  
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/word_waffle","root","Priya@1508");  
+            Statement stmt=con.createStatement(); 
+            String sql1="insert into user values("+uid+",'"+uname+"')";
+             res=stmt.executeUpdate(sql1);
+        }
+        catch(Exception e) {
+        	System.out.println(e);
+        }
+        if(res==1) {
         System.out.println("User Successfully Registered");
         gameSelection(uid, sc);
+        }
+        else {
+        	System.out.println("Duplicate id");
+        	App a=new App();
+        	a.userRegister();
+        }
     }
     private void gameSelection(int uid, Scanner sc)
     {
@@ -24,16 +68,17 @@ public class User {
             System.out.println("3 - Hangman");
             System.out.println("Enter your choice or Enter 0 to exit");
             int choice = sc.nextInt();
+            sc.nextLine();
             switch(choice)
             {
                 case 1:
                 new JumbledWord();
                 break;
                 case 2:
-                new RiddleG();
+                new RiddleG(uid);
                 break;
                 case 3:
-                new Hangman(sc);
+                new Hangman(sc,uid);
                 break;
                 case 0:
                 System.exit(0);
@@ -43,9 +88,9 @@ public class User {
             }
         }
     }
-    /*private void getHistory()
+    private void getHistory()
     {
         
-    } */  
+    }   
 }
 
